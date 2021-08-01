7aaf5575478da6c2e5fd9a1d779efbb88f5c1454
Lanny2016 <lannyca2016@gmail.com>
2021-08-01 15:55:32 +0300

    https://github.com/Lanny2016/passenger_count.git

--- Passenger_counter/index.js
+++ Passenger_counter/index.js
@@ -1,1 +1,11 @@
 //document.getElementById('count').innerText=5;
+
+
+let count = 0;
+
+function increment(){
+    count = count + 1;
+    //console.log(count);
+    document.getElementById('count').innerText = count;
+}
+
